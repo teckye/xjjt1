86c93975565277ce7247362733bbc81e500c01e4
teckye <2105279j@student.tp.edu.sg>
2022-11-23 16:40:18 +0800

    Initial Commit

---
+++ src/main/java/com/sddevops/xjjt1/App.java
@@ -0,0 +1,13 @@
+package com.sddevops.xjjt1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
